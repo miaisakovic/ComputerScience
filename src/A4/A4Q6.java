65231282940ccc0b7a53b7b036958f116c7a3715
isakm3897 <isakm3897@HRH-IC0037398.wrdsb.ca>
2019-04-11 08:47:55 -0400

    Completed A4

---
+++ src/A4/A4Q6.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A4;
+
+import java.util.Scanner;
+
+/**
+ * Outputting a Word Without the First Two A's
+ *
+ * @author isakm3897
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Create a scanner for input
+        Scanner input = new Scanner(System.in);
+
+        //Asks user for a word
+        System.out.println("Please enter a word");
+        String word = input.nextLine();
+
+        //Replaces the first two a's with nothing
+        word = word.replaceFirst("a", "");
+        word = word.replaceFirst("a", "");
+
+        //Outputs the word without the first two a's 
+        System.out.println("The word without the first two a's is " + word);
+
+    }
+
+}
